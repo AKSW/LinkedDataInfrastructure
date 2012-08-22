e92a9035617a77d8b1b9c01a34069566d942ce2a
Natanael Arndt <arndtn@gmail.com>
2012-08-22 20:02:45 +0200

    fix ping action

--- classes/Spb/PingbackController.php
+++ classes/Spb/PingbackController.php
@@ -20,7 +20,6 @@ class Spb_PingbackController extends Spb_Controller
         $logger->info('ping: source: "' . $source . '", target: "' . $target . '", comment: "' . $comment . '".');
 
         if ($source !== null && $target !== null) {
-            // TODO store and interprete ping
             $modelUri = $model->getModelIri();
             $sourceStatements = Tools::getLinkedDataResource($source, $modelUri);
 
@@ -33,7 +32,7 @@ class Spb_PingbackController extends Spb_Controller
                     $po = $memModel->getPO($target);
                     $so = $memModel->getSO($target);
 
-                    if (count($sp) > 0 && count($po) > 0 && count($so) > 0) {
+                    if (count($sp) > 0 || count($po) > 0 || count($so) > 0) {
                         $spo = array();
                         $spo[$target] = $po;
                         $spo = array_merge($spo, $sp, $so);
@@ -42,13 +41,25 @@ class Spb_PingbackController extends Spb_Controller
                         $annotateController->addNote($target, $source, $spo);
                     } else {
                         // should not happen, because we have checked this with hasResource()
+                        throw new Exception(
+                            'The ping is invalid, because there are no statements about the target at '
+                            . 'the source (second check)'
+                        );
                     }
                 } else {
                     // no statements with target found in source -> invalid ping
+                    throw new Exception(
+                        'The ping is invalid, because there are no statements about the target at '
+                        . 'the source'
+                    );
                 }
             } else {
                 //no statements found -> invalid ping
+                throw new Exception(
+                    'The ping is invalid, because there are no statements at the source'
+                );
             }
+            $template->addMessage('Ping was received successfully');
         } else {
             // no ping, show form for manual ping
             $template->addContent('templates/sendpingback.phtml');
